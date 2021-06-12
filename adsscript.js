2b2e83f80e9d2aba2379d611aaaa7d29c415611c
Sivagnanasuntharam piriyaraj <70261690+piriyaraj@users.noreply.github.com>
2021-06-13 02:31:40 +0530

    Update adsscript.js

--- adsscript.js
+++ adsscript.js
@@ -1,10 +1,3 @@
-<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
-<ins class="adsbygoogle"
-     style="display:block; text-align:center;"
-     data-ad-layout="in-article"
-     data-ad-format="fluid"
-     data-ad-client="ca-pub-5582917498984721"
-     data-ad-slot="4608500018"></ins>
 <script>
-     (adsbygoogle = window.adsbygoogle || []).push({});
+     console.log("hello piriryaraj");     
 </script>
